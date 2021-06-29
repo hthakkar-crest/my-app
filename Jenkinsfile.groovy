195077394b1c2764c5ecc44a2bec9eb4e4d409ae
harshthakkar_crest <thakkar.harsh@crestdatasys.com>
2021-06-29 12:10:08 +0530

    updated jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,17 +3,17 @@ pipeline{
     stages{
         stage('clone repo'){
             steps{
-                bat "mvn clean -f my-app"
+                sh "mvn clean -f my-app"
             }
         }
         stage('Test'){
             steps{
-                bat "mvn test -f my-app"
+                sh "mvn test -f my-app"
             }
         }
         stage('Deploy'){
             steps{
-                bat "mvn package -f my-app"
+                sh "mvn package -f my-app"
             }
         }
     }
